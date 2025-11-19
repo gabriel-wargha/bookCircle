b294c9359e5a317e2e3d2fe0bc45db6b3ad9311c
gabriel-wargha <gabriel@wargha.com>
2025-11-19 12:49:29 -0700

    adding node

--- frontend/js/firebaseConfig.js
+++ frontend/js/firebaseConfig.js
@@ -1,12 +1,5 @@
-// Import the functions you need from the Firebase CDN (ES modules)
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebaseConfig.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: 'AIzaSyCas1Cr2eVp1ukDYpgVlWz7sb4L-QkSpD8',
 	authDomain: 'bookclub-501f8.firebaseapp.com',
@@ -17,10 +10,9 @@ const firebaseConfig = {
 	measurementId: 'G-PMPBS9QSWN',
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
+// Initialize Firebase using the global firebase object
+firebase.initializeApp(firebaseConfig);
 
-// Export app, auth and config for other modules
-export { firebaseConfig, app, auth };
+// Export services for other modules
+export const auth = firebase.auth();
+export const db = firebase.firestore();
